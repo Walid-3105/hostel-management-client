e1bd867f10e4f35ee53febbe3a3c9dc96f6596e9
SA Al Walid <saawalid03@gmail.com>
2025-05-04 21:09:59 +0600

    update: navBar updated

--- src/Pages/JoinUs/Register.jsx
+++ src/Pages/JoinUs/Register.jsx
@@ -241,7 +241,7 @@ const Register = () => {
             </div>
 
             {/* Register */}
-            <div className="form-control mt-6">
+            <div className="form-control mt-6 p-10">
               <button
                 type="submit"
                 className="btn bg-[#023E8A] text-white"
